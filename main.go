a5a2b65513d80b33a89e9f570cb0af79803823ca
hedun <edunhassan1006@gmail.com>
2026-06-29 11:57:21 +0100

    drafted app skeleton

--- main.go
+++ main.go
@@ -1,1 +1,30 @@
-package todolist
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// It has four sections:
+
+// The Task struct
+// A function to load tasks from the file
+// A function to save tasks to the file
+// A main() that reads os.Args, figures out which command you ran, and calls the right logic
+
+func main() {
+
+	args := os.Args
+
+	input := args[1]
+
+	fmt.Println()
+}
+
+func loadTask(){
+
+}
+
+func saveTask(){
+	
+}
